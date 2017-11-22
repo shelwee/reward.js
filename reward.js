01b6e7aa7eba2edd279f16d33eb075aa1d06694a
Shelwee <20529004@qq.com>
2017-11-22 13:13:18 +0800

    修改样式

--- reward.js
+++ reward.js
@@ -28,10 +28,12 @@ window.onload=function(){
 		'#reward{position:relative;display:block;margin:50px auto;text-align:center;font-size:20px;line-height:40px;z-index:1;width:100px;height:40px;color:#fff;border-radius:50px;background:#E64340;letter-spacing:3px;text-indent:5px;cursor:pointer;transition:color .3s,background-color .3s}'+
 		'#reward:hover{background:#CE3C39}'+
 		'#reward:hover #reward_content{display:block}'+
-		'#reward_content{margin:0!important;z-index:1;position:absolute;width:470px;top:-280px;left:-186px;display:none;background:#fff;box-shadow:0 0 10px #ccc;border:1px solid #ccc;border-radius:5px}'+
-		'#reward_content .channel{padding-bottom: 5px;padding-left: 5px;}'+
-		'.qrcode img{width:200px;height:200px;padding:10px;margin: 5px 0;border: solid 1px #e5e5e5;}'+
-		'.channel img{width:auto;height:25px;}'+
+		'#reward_content{margin:0!important;z-index:1;position:absolute;width:465px;top:-280px;left:-186px;display:none;background:#fff;box-shadow:0 0 10px #ccc;border:1px solid #ccc;border-radius:5px}'+
+		'#reward_content table{margin:0;line-height:0;width: 0;}'+
+		'#reward_content td{padding:0;}'+
+		'#reward_content .channel{padding-bottom: 5px;padding-left: 5px;text-align: center;line-height: 0;}'+
+		'.qrcode img{width:200px;height:200px;padding:10px;margin: 5px 0;border: solid 1px #e5e5e5;box-shadow: none;}'+
+		'.channel img{width:auto;height:25px;box-shadow: none;}'+
 	'</style>';	
 	root.innerHTML = content;
 }
